61abea2b8cc30c0d4b8c304ff91991e55e694ea4
ZNahuel <nahuelzaffa@gmail.com>
2021-06-03 16:22:10 -0300

    juego terminado

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController character;
+    float ver;
+    float hor;
+
+    public float speed = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+
+        Vector3 playerMov = new Vector3(hor, 0f, ver) * speed;
+
+        character.Move(playerMov);
+    }
+}
